8bbb821c3b4de5a8b4c277829f96352172ef3e52
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-10-20 17:34:29 +0100

    set up JUnit 5 Java tests ready to create functionality

--- src/test/java/com/company/JavaTimeTravelerToolkitTest.java
+++ src/test/java/com/company/JavaTimeTravelerToolkitTest.java
@@ -1,4 +1,26 @@
+package com.company;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class JavaTimeTravelerToolkitTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void getTodaysDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLaterDatebyDays() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPreviousDatebyWeeks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTimeDifference() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
 }
