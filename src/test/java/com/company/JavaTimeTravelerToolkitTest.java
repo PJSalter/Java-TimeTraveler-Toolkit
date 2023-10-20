510026a87151b4e9d1b2d49ff9bce7276f90215b
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-10-20 17:46:53 +0100

    Added JUnit test for getTodaysDate()

--- src/test/java/com/company/JavaTimeTravelerToolkitTest.java
+++ src/test/java/com/company/JavaTimeTravelerToolkitTest.java
@@ -1,11 +1,24 @@
 package com.company;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Java Time Traveler Toolkit functionality for Date and Time")
 class JavaTimeTravelerToolkitTest {
 
     @org.junit.jupiter.api.Test
-    void getTodaysDate() {
+    @DisplayName("Positive Test case - Check whether function returns today's date successfully")
+    public void getTodaysDate() {
+        LocalDate expected = LocalDate.now();
+        LocalDate actual = JavaTimeTravelerToolkit.getTodaysDate();
+        Assertions.assertEquals(expected,actual);
     }
 
     @org.junit.jupiter.api.Test
